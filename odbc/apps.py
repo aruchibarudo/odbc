755be6ea1137a2b7cc83028d6b92f241440d9aec
aruchibarudo <archyz@gmail.com>
2018-06-25 23:49:29 +0300

    Первый коммит

---
+++ odbc/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OdbcConfig(AppConfig):
+    name = 'odbc'
---
+++ odbc/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from configparser import ConfigParser
+
+ODBC_INI = '/tmp/odbc.ini'
+
+def index(request):
+    sections = []
+    """ 
+    config = ConfigParser()
+    config.optionxform = str
+    config.readfp(open(ODBC_INI))
+    sections = config.sections
+    """
+    sections = get_ini(ODBC_INI)
+    context = { 'list' : sections }
+    return render(request,'list.html',context)
+
+def get_ini(ODBC_INI):
+    config = ConfigParser()
+    config.optionxform = str
+    config.readfp(open(ODBC_INI))
+    return config.sections
+
+
