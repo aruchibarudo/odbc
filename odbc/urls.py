23fa21566382e52dab7346944308ae310d7bb3f5
aruchibarudo <archyz@gmail.com>
2018-06-27 00:14:44 +0300

    deeping to section

--- odbc/urls.py
+++ odbc/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
  
 from . import views
  
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    path('<str:section>/', views.get_section, name='get_section'),
 ]
 
--- odbc/views.py
+++ odbc/views.py
@@ -4,6 +4,12 @@ from configparser import ConfigParser
 
 ODBC_INI = 'odbc.ini'
 
+def get_ini(ODBC_INI):
+    config = ConfigParser()
+    config.optionxform = str
+    config.readfp(open(ODBC_INI))
+    return config
+
 def index(request):
     sections = []
     """ 
@@ -12,14 +18,15 @@ def index(request):
     config.readfp(open(ODBC_INI))
     sections = config.sections
     """
-    sections = get_ini(ODBC_INI)
+    sections = ini.sections
     context = { 'list' : sections }
     return render(request,'list.html',context)
 
-def get_ini(ODBC_INI):
-    config = ConfigParser()
-    config.optionxform = str
-    config.readfp(open(ODBC_INI))
-    return config.sections
+def get_section(request, section):
+    options = ini[section]
+    values = ini[section].values()
+    context = { 'items' : options, 'values' : values }
+    return render(request, 'section.html',context)
 
+ini = get_ini(ODBC_INI)
 
